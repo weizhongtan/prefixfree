903f18ce8e5b15f8d22a51fd28db135b4ed4c23b
Wei-Zhong Tan <weizhong.tan@amigotechnology.com>
2018-05-16 20:07:55 +0100

    remove unnecessary stylefix methods

--- prefixfree.js
+++ prefixfree.js
@@ -4,155 +4,17 @@
  * MIT license
  */
 
-var StyleFix = (function(StyleFix){
+/**
+ * PrefixFree
+ */
+var PrefixFree = (function(root){
 
-if(!window.addEventListener) {
+if(!window.getComputedStyle) {
 	return;
 }
 
-var self = StyleFix = {
-	link: function(link) {
-		var url = link.href || link.getAttribute('data-href');
-		try {
-			// Ignore stylesheets with data-noprefix attribute as well as alternate stylesheets or without (data-)href attribute
-			if(!url || link.rel !== 'stylesheet' || link.hasAttribute('data-noprefix')) {
-				return;
-			}
-		}
-		catch(e) {
-			return;
-		}
-
-		var base = url.replace(/[^\/]+$/, ''),
-		    base_scheme = (/^[a-z]{3,10}:/.exec(base) || [''])[0],
-		    base_domain = (/^[a-z]{3,10}:\/\/[^\/]+/.exec(base) || [''])[0],
-		    base_query = /^([^?]*)\??/.exec(url)[1],
-		    parent = link.parentNode,
-		    xhr = new XMLHttpRequest(),
-		    process;
-
-		xhr.onreadystatechange = function() {
-			if(xhr.readyState === 4) {
-				process();
-			}
-		};
-
-		process = function() {
-				var css = xhr.responseText;
-
-				if(css && link.parentNode && (!xhr.status || xhr.status < 400 || xhr.status > 600)) {
-					css = self.fix(css, true, link);
-
-					// Convert relative URLs to absolute, if needed
-					if(css && base) {
-						css = css.replace(/url\(\s*?((?:"|')?)(.+?)\1\s*?\)/gi, function($0, quote, url) {
-							if(/^([a-z]{3,10}:|#)/i.test(url)) { // Absolute & or hash-relative
-								return $0;
-							}
-							else if(/^\/\//.test(url)) { // Scheme-relative
-								// May contain sequences like /../ and /./ but those DO work
-								return 'url("' + base_scheme + url + '")';
-							}
-							else if(/^\//.test(url)) { // Domain-relative
-								return 'url("' + base_domain + url + '")';
-							}
-							else if(/^\?/.test(url)) { // Query-relative
-								return 'url("' + base_query + url + '")';
-							}
-							else {
-								// Path-relative
-								return 'url("' + base + url + '")';
-							}
-						});
-
-						// behavior URLs shoudn’t be converted (Issue #19)
-						// base should be escaped before added to RegExp (Issue #81)
-						var escaped_base = base.replace(/([\\\^\$*+[\]?{}.=!:(|)])/g,"\\$1");
-						css = css.replace(RegExp('\\b(behavior:\\s*?url\\(\'?"?)' + escaped_base, 'gi'), '$1');
-						}
-
-					var style = document.createElement('style');
-					style.textContent = '/*# sourceURL='+link.getAttribute('href')+' */\n/*@ sourceURL='+link.getAttribute('href')+' */\n' + css;
-					style.media = link.media;
-					style.disabled = link.disabled;
-					style.setAttribute('data-href', link.getAttribute('href'));
-
-					if(link.id) style.id = link.id;
-
-					parent.insertBefore(style, link);
-					parent.removeChild(link);
-
-					style.media = link.media; // Duplicate is intentional. See issue #31
-				}
-		};
-
-		try {
-			xhr.open('GET', url);
-			xhr.send(null);
-		} catch (e) {
-			// Fallback to XDomainRequest if available
-			if (typeof XDomainRequest != "undefined") {
-				xhr = new XDomainRequest();
-				xhr.onerror = xhr.onprogress = function() {};
-				xhr.onload = process;
-				xhr.open("GET", url);
-				xhr.send(null);
-			}
-		}
-
-		link.setAttribute('data-inprogress', '');
-	},
-
-	styleElement: function(style) {
-		if (style.hasAttribute('data-noprefix')) {
-			return;
-		}
-		var disabled = style.disabled;
-
-		style.textContent = self.fix(style.textContent, true, style);
-
-		style.disabled = disabled;
-	},
-
-	styleAttribute: function(element) {
-		var css = element.getAttribute('style');
-
-		css = self.fix(css, false, element);
-
-		element.setAttribute('style', css);
-	},
-
-	process: function() {
-		// Linked stylesheets
-		$('link[rel="stylesheet"]:not([data-inprogress])').forEach(StyleFix.link);
-
-		// Inline stylesheets
-		$('style').forEach(StyleFix.styleElement);
-
-		// Inline styles
-		$('[style]').forEach(StyleFix.styleAttribute);
-
-		var event = document.createEvent('Event');
-		event.initEvent('StyleFixProcessed', true, true);
-		document.dispatchEvent(event);
-
-	},
-
-	register: function(fixer, index) {
-		(self.fixers = self.fixers || [])
-			.splice(index === undefined? self.fixers.length : index, 0, fixer);
-	},
-
-	fix: function(css, raw, element) {
-		if(self.fixers) {
-		  for(var i=0; i<self.fixers.length; i++) {
-			css = self.fixers[i](css, raw, element) || css;
-		  }
-		}
-
-		return css;
-	},
-
+// only required methods from StyleFix
+var StyleFix = {
 	camelCase: function(str) {
 		return str.replace(/-([a-z])/g, function($0, $1) { return $1.toUpperCase(); }).replace('-','');
 	},
@@ -161,34 +23,6 @@ var self = StyleFix = {
 		return str.replace(/[A-Z]/g, function($0) { return '-' + $0.toLowerCase() });
 	}
 };
-
-/**************************************
- * Process styles
- **************************************/
-(function(){
-	setTimeout(function(){
-		$('link[rel="stylesheet"]').forEach(StyleFix.link);
-	}, 10);
-
-	document.addEventListener('DOMContentLoaded', StyleFix.process, false);
-})();
-
-function $(expr, con) {
-	return [].slice.call((con || document).querySelectorAll(expr));
-}
-
-return self;
-
-})();
-
-/**
- * PrefixFree
- */
-var PrefixFree = (function(root, StyleFix){
-
-if(!StyleFix || !window.getComputedStyle) {
-	return;
-}
 
 // Private helper
 function fix(what, before, after, replacement, css) {
@@ -523,10 +357,8 @@ self.valueProperties = [
 // Add class for current prefix
 root.className += ' ' + self.prefix;
 
-StyleFix.register(self.prefixCSS);
-
 return self;
 
-})(document.documentElement, StyleFix);
+})(document.documentElement);
 
 module.exports = PrefixFree;
